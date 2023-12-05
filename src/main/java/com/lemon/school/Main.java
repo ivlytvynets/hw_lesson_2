896c2131324cd714fe7d070d4195125bded0d1be
Viktoria <vika28292000@gmail.com>
2023-12-05 17:25:24 +0200

    implemented some feature

--- src/main/java/com/lemon/school/Main.java
+++ src/main/java/com/lemon/school/Main.java
@@ -2,7 +2,18 @@ package com.lemon.school;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        String greet = "Hello Bob";
+        System.out.println(greet);
 
-  }
+        int a = 280;
+        int b = 140;
+        int divideNum = a / b;
+        System.out.println(divideNum);
+
+
+
+    }
 }
+
+
