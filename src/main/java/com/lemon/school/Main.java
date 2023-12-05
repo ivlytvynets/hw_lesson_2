dc45bd656a0ce97cca65e58b081fd36df71d0110
Nikita Yurkovskyi <yrkovskiynik@gmail.com>
2023-12-05 22:05:39 +0200

    HomeWork2 Nikita Yurkovskyi

--- src/main/java/com/lemon/school/Main.java
+++ src/main/java/com/lemon/school/Main.java
@@ -4,5 +4,29 @@ public class Main {
 
   public static void main(String[] args) {
 
+    String greet = greet("Hello Pen");
+    System.out.println(greet);
+
+    int divide = divide(10,5);
+    System.out.println(divide);
+
+    boolean res = isEven(8);
+    System.out.println(res);
+
+    print(greet("Hello Pen"));
+  }
+
+  public static String greet(String greet){
+    return greet;
+  }
+
+  public static int divide(int a, int b){
+    return a/b;
+  }
+  public static boolean isEven(int number){
+    return number % 2 == 0;
+  }
+  public static void print(String m){
+     System.out.println(m);
   }
 }
