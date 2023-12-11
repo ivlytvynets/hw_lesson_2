f2d0492a26015f28b1d1a379af7e30453690edd8
Viktoria <vika28292000@gmail.com>
2023-12-11 15:19:27 +0200

    implemented some feature

--- src/main/java/com/lemon/school/Main.java
+++ src/main/java/com/lemon/school/Main.java
@@ -3,7 +3,8 @@ package com.lemon.school;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(getgreet());
+        String s = "Name";
+        System.out.println(Name());
 
         int a = 280;
         int b = 140;
@@ -11,53 +12,28 @@ public class Main {
         int b1 = 230;
         int a2 = 950;
         int b2 = 240;
-        int divide = getDivide(a, b);
-        int divide1 = getDivide(a1, b1);
-        int divide2 = getDivide(a2, b2);
-        System.out.println("Результат:" + divide + "Результат:" + divide1 + "Результат:" + divide2);
+        int divide = Divide(a, b);
 
+        System.out.println("Результат:" + divide);
 
-        int num1 = 33;
+        int num1 = 40;
         System.out.println(isEven(num1));
-
 
         String Hello = "Hello";
         String Bob = "Bob";
         System.out.print(Hello);
         System.out.print(Bob);
-
-
     }
 
-    public static String getgreet() {
+    public static String Name() {
         return "Hello Bob";
-    }
-
-    public static int getDivide(int a, int b) {
-        return a / b;
     }
 
     public static boolean isEven(int num1) {
         return num1 % 2 == 0;
     }
 
-    int getDivide1(int a1, int b1) {
-        return a1 / b1;
-    }
-
-    int getDivide2(int a2, int b2) {
-        return a2 / b2;
+    public static int Divide(int a, int b) {
+        return a / b;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
